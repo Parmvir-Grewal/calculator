eeb1147da97d9f204926c9ab102af58ffa914a02
Parmvir Grewal <parmg100@gmail.com>
2021-07-30 11:58:25 -0400

    Create basic math operator functions

---
+++ app.js
@@ -0,0 +1,15 @@
+function add(n, n1) {
+    return n + n1;
+}
+
+function subtract(n, n1) {
+    return n - n1;
+}
+
+function multiply(n, n1) {
+    return n * n1;
+}
+
+function divide(n, n1) {
+    return n / n1;
+}

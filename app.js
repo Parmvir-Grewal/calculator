6d3ed2a246616c891ac40f4b71e1520f43ec4a19
Parmvir Grewal <parmg100@gmail.com>
2021-07-30 12:04:02 -0400

    Create math operator function

--- app.js
+++ app.js
@@ -13,3 +13,20 @@ function multiply(n, n1) {
 function divide(n, n1) {
     return n / n1;
 }
+
+function operate(operator, n, n1) {
+    switch (operator) {
+        case "+":
+            return add(n, n1);
+            break;
+        case "-":
+            return subtract(n, n1);
+            break;
+        case "*":
+            return multiply(n, n1);
+            break;
+        case "/":
+            return divide(n, n1);
+            break;
+    }
+}
